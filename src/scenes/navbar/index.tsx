f965ee3a14c468fc5b83054e6c30816862a92f9a
youessaitch <ayushkumar0077680@gmail.com>
2024-08-19 13:13:24 +0530

    first commit

---
+++ src/scenes/navbar/index.tsx
@@ -0,0 +1,67 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import PixIcon from "@mui/icons-material/Pix";
+import { Box, Typography, useTheme } from "@mui/material";
+import FlexBetween from "@/components/FlexBetween";
+
+type Props = {};
+
+const Navbar = (props: Props) => {
+  const { palette } = useTheme();
+  const [selected, setSelected] = useState("dashboard");
+  return (
+    <FlexBetween mb="0.25rem" p="0.5rem 0rem" color={palette.grey[300]}>
+      {/* LEFT SIDE */}
+      <FlexBetween gap="0.75rem">
+        <PixIcon sx={{ fontSize: "28px" }} />
+        <Typography variant="h4" fontSize="16px">
+          FinAnalyzr
+        </Typography>
+      </FlexBetween>
+
+      {/* RIGHT SIDE */}
+      <FlexBetween gap="2rem">
+        <Box sx={{ "&:hover": { color: palette.primary[100] } }}>
+          <Link
+            to="/"
+            onClick={() => setSelected("dashboard")}
+            style={{
+              color: selected === "dashboard" ? "inherit" : palette.grey[700],
+              textDecoration: "inherit",
+            }}
+          >
+            dashboard
+          </Link>
+        </Box>
+        <Box sx={{ "&:hover": { color: palette.primary[100] } }}>
+          <Link
+            to="/predictions"
+            onClick={() => setSelected("predictions")}
+            style={{
+              color: selected === "predictions" ? "inherit" : palette.grey[700],
+              textDecoration: "inherit",
+            }}
+          >
+            predictions
+          </Link>
+        </Box>
+        <Box sx={{ "&:hover": { color: palette.primary[100] } }}>
+          <a
+            href="https://www.linkedin.com/in/ayush-kr-iitkgp/" // Replace with your LinkedIn URL
+            target="_blank" // Opens the link in a new tab
+            rel="noopener noreferrer" // Security best practice
+            style={{
+              color: selected === "contact" ? "inherit" : palette.grey[700],
+              textDecoration: "inherit",
+            }}
+            onClick={() => setSelected("contact")}
+          >
+            contact
+          </a>
+        </Box>
+      </FlexBetween>
+    </FlexBetween>
+  );
+};
+
+export default Navbar;
